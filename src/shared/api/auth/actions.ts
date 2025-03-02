3ed6b6c9d5cd9c4d381b0021a7451190c22b8be9
Иван <burack.iw@yandex.ru>
2025-03-02 23:16:15 +0300

    Обновил завпрос на авторизацию

--- src/shared/api/auth/actions.ts
+++ src/shared/api/auth/actions.ts
@@ -1,12 +1,22 @@
-'use server';
-
 import { TypeLogin, TypeRegistration } from "@/features/auth/schemas";
+import { API_URL } from "@/shared/constants";
 
-export async function register( body: TypeRegistration) {
+export async function register( body: TypeRegistration ) {
 	console.log(body)
 }
 
-export async function login(body: TypeLogin) {
-	console.log(body)
-	throw Error('Не удалось залогиниться')
+export async function login( body: TypeLogin ) {
+	try {
+		const resp = await fetch(`${ API_URL }/login`, {});
+
+		if ( !resp.ok ) {
+			return Promise.reject('Ошибка сервера. Не возможно выполнить запрос [блок try]');
+		}
+
+		const data = await resp.json();
+		return Promise.resolve(data);
+
+	} catch ( err ) {
+		return Promise.reject('Ошибка сервера. Не возможно выполнить запрос [блок catch]');
+	}
 }
