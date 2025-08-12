0a2cb060f486ad1af57ba52c3ce7e656411af82c
a.fugin <a.fugin@zaryad.tech>
2025-08-12 22:50:53 +0300

    page earning

--- src/app/(child)/child/earnings/page.tsx
+++ src/app/(child)/child/earnings/page.tsx
@@ -1,7 +1,19 @@
 'use client';
 
 import React from 'react';
-import { CardReward, Heading, Helper, SectionWhite, Text, TextWithButton, Button, CheckboxWithLabel } from '@components';
+import {
+	CardReward,
+	Heading,
+	Helper,
+	SectionWhite,
+	Text,
+	TextWithButton,
+	Button,
+	CheckboxWithLabel,
+	Textarea,
+} from '@components';
+import Image from 'next/image';
+import image from '@assets/images/child/cleaning-service.webp';
 
 const Earnings = () => {
   const [checked, setChecked] = React.useState({
@@ -10,52 +22,76 @@ const Earnings = () => {
 		clean: true,
 		vacuumСleaning: false,
 		cleaningRoom: false,
+		washSocks: false,
+		takeTrash: false,
+		walkDog: false,
   });
 
   return (
     <>
-      <CardReward text={'Домашнее задание от мамы'} reward={'300'}/>
-      <CardReward text={'Домашнее задание от папы'} reward={'500'} status={'выполнено'} variant={'green'}/>
-      <CardReward text={'Домашнее задание от бабушки'} reward={'100'} status={'просрочено'} variant={'red'}/>
-      <SectionWhite overflow='overflow-visible'>
-        <div className="mb-[110]">
-          <Text data={{ text: 'Домашнее задание от мамы от ', tag: 'span' }} cn={{ size: 'text-body-1', weight: 'font-medium' }} />
-          <Text data={{ text: '01.01.2025 ', tag: 'span' }} cn={{ size: 'text-body-1', weight: 'font-medium' }} />
-        </div>
+			<div className={"flex flex-col gap-[50]"}>
+				<CardReward text={'Домашнее задание от мамы'} reward={'300'}/>
+				<SectionWhite overflow='overflow-visible'>
+					<div className="mb-[110]">
+						<Text data={{ text: 'Домашнее задание от мамы от ', tag: 'span' }} cn={{ size: 'text-body-1', weight: 'font-medium' }} />
+						<Text data={{ text: '01.01.2025 ', tag: 'span' }} cn={{ size: 'text-body-1', weight: 'font-medium' }} />
+					</div>
 
-				<div>
-					<CheckboxWithLabel
-						label="вымыть посуду"
-						checked={checked.dishes}
-						onChange={v => setChecked(prev => ({ ...prev, dishes: v }))}
-					/>
-					<CheckboxWithLabel
-						label="полить цветы"
-						checked={checked.flowers}
-						onChange={v => setChecked(prev => ({ ...prev, flowers: v }))}
-					/>
-					<CheckboxWithLabel
-						label="убрать со стола"
-						checked={checked.clean}
-						onChange={v => setChecked(prev => ({ ...prev, clean: v }))}
-					/>
-					<CheckboxWithLabel
-						label="пропылесосить комнату"
-						checked={checked.vacuumСleaning}
-						onChange={v => setChecked(prev => ({ ...prev, vacuumСleaning: v }))}
-					/>
-					<CheckboxWithLabel
-						label="убраться в комнате "
-						checked={checked.cleaningRoom}
-						onChange={v => setChecked(prev => ({ ...prev, cleaningRoom: v }))}
-					/>
-				</div>
+					<div className={"flex flex-wrap gap-y-[50] mb-[40]"}>
+						<CheckboxWithLabel
+							className={"basic-250"}
+							label="вымыть посуду"
+							checked={checked.dishes}
+							onChange={v => setChecked(prev => ({ ...prev, dishes: v }))}
+						/>
+						<CheckboxWithLabel
+							label="полить цветы"
+							checked={checked.flowers}
+							onChange={v => setChecked(prev => ({ ...prev, flowers: v }))}
+						/>
+						<CheckboxWithLabel
+							label="убрать со стола"
+							checked={checked.clean}
+							onChange={v => setChecked(prev => ({ ...prev, clean: v }))}
+						/>
+						<CheckboxWithLabel
+							label="пропылесосить комнату"
+							checked={checked.vacuumСleaning}
+							onChange={v => setChecked(prev => ({ ...prev, vacuumСleaning: v }))}
+						/>
+						<CheckboxWithLabel
+							label="убраться в комнате "
+							checked={checked.cleaningRoom}
+							onChange={v => setChecked(prev => ({ ...prev, cleaningRoom: v }))}
+						/>
+						<CheckboxWithLabel
+							label="постирать носки"
+							checked={checked.washSocks}
+							onChange={v => setChecked(prev => ({ ...prev, washSocks: v }))}
+						/>
+						<CheckboxWithLabel
+							label="вынести мусор"
+							checked={checked.takeTrash}
+							onChange={v => setChecked(prev => ({ ...prev, takeTrash: v }))}
+						/>
+						<CheckboxWithLabel
+							label="выгулять собаку"
+							checked={checked.walkDog}
+							onChange={v => setChecked(prev => ({ ...prev, walkDog: v }))}
+						/>
+					</div>
 
-        <Helper cn={{width: 'full'}}>
-          <Text data={{text: 'здесь ты можешь написать комментарий', tag: 'p'}} />
-        </Helper>
-        <Button >Выполнить</Button>
-      </SectionWhite>
+					<Helper cn={{width: 'full'}}>
+						<Textarea placeholder="здесь ты можешь написать комментарий " />
+					</Helper>
+					<div className={"flex justify-end mt-10"}>
+						<Button >Выполнить</Button>
+					</div>
+				</SectionWhite>
+				<CardReward text={'Домашнее задание от папы'} reward={'500'} status={'выполнено'} variant={'green'}/>
+				<CardReward text={'Домашнее задание от бабушки'} reward={'100'} status={'просрочено'} variant={'red'}/>
+				<Image src={image} alt="Приветствие" width={736} />
+			</div>
     </>
   );
 };
