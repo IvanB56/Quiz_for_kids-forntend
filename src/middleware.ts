2fc8a93491646e7cb204fc8036a5791677966a6a
IvanB56 <burack.iw@yandex.ru>
2025-09-04 00:15:39 +0300

    правки слайдера

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,32 +1,32 @@
 import {NextRequest, NextResponse} from 'next/server'
-import {checkAuth, getUser} from "@features";
+import {checkAuth} from "@features";
 
 
 export async function middleware(request: NextRequest) {
 	const {status} = await checkAuth();
 
-	if (status === 200) {
-		const {user} = await getUser();
-
-		if (request.nextUrl.pathname === '/') {
-			if (user?.data.type === 'Sponsor') {
-				return NextResponse.redirect(new URL('/profile/rules', request.url))
-			}
-
-			if (user?.data.type === 'Student') {
-				return NextResponse.redirect(new URL('/child/rules', request.url))
-			}
-		}
-	} else {
-		const protectedPaths = ['/child', '/profile', '/settings']
-		const isProtectedPath = protectedPaths.some(path =>
-			request.nextUrl.pathname.startsWith(path)
-		)
-
-		if (isProtectedPath && status !== 200) {
-			return NextResponse.redirect(new URL('/?login=true', request.url))
-		}
-	}
+	// if (status === 200) {
+	// 	const {user} = await getUser();
+	//
+	// 	if (request.nextUrl.pathname === '/') {
+	// 		if (user?.data.type === 'Sponsor') {
+	// 			return NextResponse.redirect(new URL('/profile/rules', request.url))
+	// 		}
+	//
+	// 		if (user?.data.type === 'Student') {
+	// 			return NextResponse.redirect(new URL('/child/rules', request.url))
+	// 		}
+	// 	}
+	// } else {
+	// 	const protectedPaths = ['/child', '/profile', '/settings']
+	// 	const isProtectedPath = protectedPaths.some(path =>
+	// 		request.nextUrl.pathname.startsWith(path)
+	// 	)
+	//
+	// 	if (isProtectedPath && status !== 200) {
+	// 		return NextResponse.redirect(new URL('/?login=true', request.url))
+	// 	}
+	// }
 
 	return NextResponse.next();
 }
