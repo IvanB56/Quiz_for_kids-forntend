34115403df7a3aa43b1ca41e8c69f28e9317e8e4
a.fugin <a.fugin@zaryad.tech>
2025-09-23 19:18:54 +0300

    add aside

--- src/app/(child)/layout.tsx
+++ src/app/(child)/layout.tsx
@@ -2,31 +2,96 @@ import React from 'react';
 import {ProfileChildProvider} from "@shared";
 import {ProfileAside} from "@widgets";
 import '@assets/styles/child.scss'
+import { AppSidebar, SidebarInset, SidebarProvider, SidebarTrigger } from '@components';
 
 
 export default async function AuthLayout({children}: { children: React.ReactNode }) {
-	const pagesLink = [
-		{name: 'Игровой договор', href: '/child/rules'},
-		{name: 'Вопросы от Алины. Приветствие', href: '/child/greetings'},
-		{name: 'Вопросы от Алины', href: '/child/tasks'},
-		{name: 'Задания от Алины', href: '/child/education'},
-		{name: 'Обучение от Алины', href: '/child/homework'},
-		{name: 'Заработай реальные деньги', href: '/child/earnings'},
-		{name: 'Мой сейф', href: '/child/wallet'},
-		{name: 'Мой сейф 2', href: '/child/wallet2'},
-		{name: 'Настройки личного кабинета', href: '/child/settings'},
-	];
+	{/*const pagesLink = [*/}
+	{/*	{name: 'Игровой договор', href: '/child/rules'},*/}
+	// 	{name: 'Вопросы от Алины. Приветствие', href: '/child/greetings'},
+	// 	{name: 'Вопросы от Алины', href: '/child/tasks'},
+	// 	{name: 'Задания от Алины', href: '/child/education'},
+
+	// 	{name: 'Обучение от Алины', href: '/child/homework'},
+	// 	{name: 'Заработай реальные деньги', href: '/child/earnings'},
+	// 	{name: 'Мой сейф', href: '/child/wallet'},
+	// 	{name: 'Мой сейф 2', href: '/child/wallet2'},
+	// 	{name: 'Настройки личного кабинета', href: '/child/settings'},
+	// ];
+
+	const pagesLink = {
+		user: {
+			name: "Ivan",
+			email: "admin@admin.com",
+			avatar: "/avatars/shadcn.jpg",
+		},
+		navMain: [
+			{
+				title: "Приложение",
+				items: [
+					{
+						title: "Игровой договор",
+						url: "/child/rules",
+						icon: 'ReceiptText',
+						isActive: true,
+					},
+					{
+						title: "Вопросы от Алины. Приветствие",
+						url: "/child/greetings",
+						icon: 'Meh'
+					},
+					{
+						title: "Вопросы от Алины",
+						url: "/child/tasks",
+						icon: 'BookCheck'
+					},
+					{
+						title: "Задания от Алины",
+						url: "/child/education",
+						icon: 'NotebookPen'
+					},
+					{
+						title: "Обучение от Алины",
+						url: "/child/homework",
+						icon: 'GraduationCap'
+					},
+					{
+						title: "Заработай реальные деньги",
+						url: "/child/earnings",
+						icon: 'Banknote'
+					},
+					{
+						title: "Мой сейф",
+						url: "/child/wallet",
+						icon: 'PiggyBank'
+					},
+					{
+						title: "Мой сейф 2",
+						url: "/child/wallet2",
+						icon: 'PiggyBank'
+					},
+					{
+						title: "Настройки личного кабинета",
+						url: "/child/settings",
+						icon: 'SettingsIcon'
+					},
+				]
+			},
+		]
+	}
 
 	return (
 		<ProfileChildProvider>
-			<main className="child-main">
-				<div className="flex">
-					<ProfileAside pagesLink={pagesLink}/>
-					<div className={'flex flex-col w-full p-[1.875rem] sm:p-[3.75rem] h-[100dvh] overflow-auto'}>
+			<SidebarProvider>
+				<AppSidebar {...pagesLink}/>
+				<SidebarInset className="bg-cyan-light">
+					<SidebarTrigger
+						className="[&_svg]:size-6 [&_svg]:rotate-180 absolute bg-sidebar !rounded-l-none left-0 min-md:hidden"/>
+					<div className="flex flex-1 flex-col gap-4 p-10 max-md:p-8 bg-cyan-light h-full overflow-auto">
 						{children}
 					</div>
-				</div>
-			</main>
+				</SidebarInset>
+			</SidebarProvider>
 		</ProfileChildProvider>
 	);
 }
